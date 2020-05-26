8faeea75970ad70acd9d3e5cb9caf42f6392f588
artemb <a.bobrovskij@gmail.com>
2020-05-26 08:15:37 +0100

    First lesson for group 5

---
+++ maze_lesson/maze.py
@@ -0,0 +1,20 @@
+from maze_lesson._lib import MazeTurtle
+
+# Лабиринты от 0 до 7
+turtle = MazeTurtle(maze=0)
+
+# Вставляйте свой код между двумя горизонтальными линиями
+#
+# Команды:
+#
+# turtle.step_right()  - шаг вправо
+# turtle.step_down()   - шаг вниз
+# turtle.step_left()   - шаг влево
+# turtle.step_up()     - шаг вверх
+# ---------------------------------
+
+
+
+# ---------------------------------
+
+turtle.mainloop()
