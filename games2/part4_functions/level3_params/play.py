98cc898e8863132b20467a230ccff7062b3f9135
artemb <a.bobrovskij@gmail.com>
2020-07-09 19:16:12 +0100

    Fix

--- games2/part4_functions/level3_params/play.py
+++ games2/part4_functions/level3_params/play.py
@@ -1,5 +1,5 @@
-from games._lib.gamelib import *
-from games.part4_functions.level3_params._data import conf
+from games2._lib.gamelib import *
+from games2.part4_functions.level3_params._data import conf
 
 create_game(conf)
 
